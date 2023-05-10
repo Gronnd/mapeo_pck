ec927d1b98a8aa756968a8c6b70abc0b535f4751
Jorge <77586212+Gronnd@users.noreply.github.com>
2023-05-11 01:01:20 +0200

    Update .gitignore

--- R/crear_shp.R
+++ R/crear_shp.R
@@ -1,8 +1,8 @@
 #' crear_shp()
 #'
-#'Función que permite generar un nuevo shp según los parámetros especificados
+#' Función que permite generar un nuevo shp según los parámetros especificados
 #' @param shp_data datos del shp
-#' @param municipio municipio a seleccionar
+#' @param criterio_seleccion vector o lista con nombres, donde el nombre de cada elemento es el nombre de la columna y el valor es el valor a seleccionar
 #' @param agrupar_por columnas por las que se agrupará el shp
 #' @param columnas_adicionales columnas adicionales a incluir en el shp
 #'
@@ -12,18 +12,23 @@
 #' @importFrom dplyr %>% group_by summarise
 #' @importFrom sf st_union st_transform
 
-#'
-#'
-crear_shp <- function(shp_data, municipio, agrupar_por, columnas_adicionales = NULL) {
+crear_shp <- function(shp_data, criterio_seleccion, agrupar_por, columnas_adicionales = NULL) {
   if (is.null(columnas_adicionales)) {
     columnas_adicionales <- c()
   }
-  
-  municipio_seleccionado <- shp_data[shp_data$NMUN %in% municipio, c("NMUN", agrupar_por, "geometry", columnas_adicionales)]
-  municipio_agrupado <- municipio_seleccionado %>%
+
+  if (is.vector(criterio_seleccion)) {
+    criterio_seleccion <- list(criterio_seleccion)
+  }
+
+  for (nombre in names(criterio_seleccion)) {
+    shp_data <- shp_data[shp_data[[nombre]] %in% criterio_seleccion[[nombre]], ]
+  }
+
+  datos_agrupados <- shp_data %>%
     dplyr::mutate(geometry = sf::st_transform(geometry, '+proj=longlat +datum=WGS84')) %>%
-    dplyr::group_by(dplyr::across(c(NMUN, agrupar_por, columnas_adicionales))) %>%
+    dplyr::group_by(dplyr::across(c(agrupar_por, columnas_adicionales))) %>%
     dplyr::summarise(geometry = sf::st_union(geometry), .groups = "drop")
-  
-  return(municipio_agrupado)
+
+  return(datos_agrupados)
 }
